75f80530fa1e297b3c015fa06e06978f2f559262
hanzala bawany <hanzalabawany0@gmail.com>
2024-11-13 22:45:04 +0500

    responsive and some function of dice game

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let player1Name = prompt("player 1 enter name").toUpperCase().trim();
- while (player1Name.length > 14){
-    player1Name = prompt("Name Can Not Greater Then 14").toUpperCase().trim();
- }
-let player2Name = prompt("player 2 enter name").toUpperCase().trim();
-while (player2Name.length > 14){
-    player2Name = prompt("Name Can Not Greater Then 14").toUpperCase().trim();
- }
+let player1Name = prompt("player 1 enter name").trim();
+while (player1Name.length > 14) {
+    player1Name = prompt("Name Can Not Greater Then 14").trim();
+}
+let player2Name = prompt("player 2 enter name").trim();
+while (player2Name.length > 14) {
+    player2Name = prompt("Name Can Not Greater Then 14").trim();
+}
 
-document.body.querySelector(".name1").innerHTML=player1Name
-document.body.querySelector(".name2").innerHTML=player2Name
+
+document.body.querySelector(".name1").innerHTML = player1Name
+document.body.querySelector(".name2").innerHTML = player2Name
 
 alert('player 1 : ' + player1Name + " start")
 
@@ -91,14 +92,17 @@ function hold() {
 
         totalScore_1 += currentScore1;
         document.body.querySelector(".totalScore1").innerHTML = totalScore_1;
-        if(totalScore_1 >= 100){
-            alert(`${player1Name} YOU WIN`)
+        if (totalScore_1 >= 100) {
+            // alert(`${player1Name} YOU WIN`)
+
+            document.querySelector(".winModal").querySelector(".wName").innerHTML = player1Name
+            modalOpen()
 
             currentScore1 = 0;
             document.body.querySelector(".currentScore1").innerHTML = "00"
             currentScore2 = 0;
             document.body.querySelector(".currentScore2").innerHTML = "00"
-        
+
             totalScore_1 = 0;
             document.body.querySelector(".totalScore1").innerHTML = '00';
             totalScore_2 = 0;
@@ -122,19 +126,22 @@ function hold() {
 
         totalScore_2 += currentScore2;
         document.body.querySelector(".totalScore2").innerHTML = totalScore_2;
-        if(totalScore_2 >= 100){
-            alert(`${player2Name} YOU WIN`)
+        if (totalScore_2 >= 100) {
+            // alert(`${player2Name} YOU WIN`)
+
+            document.querySelector(".winModal").querySelector(".wName").innerHTML = player2Name
+            modalOpen()
 
             currentScore1 = 0;
             document.body.querySelector(".currentScore1").innerHTML = "00"
             currentScore2 = 0;
             document.body.querySelector(".currentScore2").innerHTML = "00"
-        
+
             totalScore_1 = 0;
             document.body.querySelector(".totalScore1").innerHTML = '00';
             totalScore_2 = 0;
             document.body.querySelector(".totalScore2").innerHTML = '00';
-            
+
             return;
         }
 
@@ -165,12 +172,25 @@ function newGame() {
     totalScore_2 = 0;
     document.body.querySelector(".totalScore2").innerHTML = '00';
 
-    player1Name = prompt("player 1 name").toUpperCase().trim();
-    player2Name = prompt("player 2 name").toUpperCase().trim();
+    player1Name = prompt("player 1 name").trim();
+    player2Name = prompt("player 2 name").trim();
 
     document.body.querySelector(".name1").innerHTML = player1Name
     document.body.querySelector(".name2").innerHTML = player2Name
 
     alert('player 1 : ' + player1Name + " start")
 
+}
+
+//  modal close function
+
+function modalClose() {
+    document.body.querySelector(".parentWinModal").classList.toggle("modalOpen")
+
+}
+
+//  open close function
+
+function modalOpen() {
+    document.body.querySelector(".parentWinModal").classList.toggle("modalOpen")
 }
